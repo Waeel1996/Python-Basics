5570cd65459da1ddfcebf9fcf52ad747ac354f85
Waeel1996 <waeelalhamad200@gmail.com>
2023-06-17 22:46:30 +0200

    code update

---
+++ paython_Basics 2.py
@@ -0,0 +1,25 @@
+#format str:
+
+#exp:
+s='welcome'
+s="welcome"
+s='''welcome'''
+f_name = 'ahmad'
+l_name = 'ali'
+print (f"{f_name} {l_name}")
+
+#\n to jump down a line:
+
+print('ahmad \nwael')
+
+#\t to leave a space:
+
+print('mahmod \twael')
+
+#text funktion:
+
+text = 'my name is wael'
+print(text.upper())
+print(text.lower())
+print(text.title())
+
