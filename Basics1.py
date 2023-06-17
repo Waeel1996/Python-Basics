2a70cb150328dbeb0f585560c457ee60935e3167
Waeel1996 <waeelalhamad200@gmail.com>
2023-06-17 03:27:16 +0200

    all and any

--- Basics1.py
+++ Basics1.py
@@ -27,3 +27,20 @@ for x in range (1,11):
     for y in range (1,11):
         print (f'{x} X {y} = {x*y}')
     print('--------------------')
+
+#all and any
+#instade of typing many ands OR ors, you can use all for many ands, and any for many ors
+x=5
+y=2
+z=6
+
+#if x==5 and y==2 and z==6:
+    #print (' welcome ')
+if all([x==5 , y==2 , z==6]):
+    print (' welcome ')
+
+#if x==5 or y==2 or z==6:
+    #print (' welcome ')
+
+if any([x==5 , y==2 , z==6]):
+     print (' unwelcome ')
