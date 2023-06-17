cca8668e2f825ff01f079ad5c8a1191cad22e4c6
Waeel1996 <waeelalhamad200@gmail.com>
2023-06-17 03:13:46 +0200

    loop added

--- Basics1.py
+++ Basics1.py
@@ -1,5 +1,22 @@
+# some Basics instructions on how to start with Python
+
+#cods I need to remember
+#if and else
+
 x=5
-if x<5:
+if x<4:
     print(x)
 else:
     print('unknown')
+
+#loop :
+
+x=1
+while x<=5:
+    y=1
+    while y<=10:
+        print (f'{x} X {y} = {x*y}')
+        y+=1
+        
+    x+=1
+    print('--------------------')
