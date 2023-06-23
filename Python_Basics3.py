3f7e708fc5d648ee22557cf6762f2c30aabea0d6
Waeel1996 <waeelalhamad200@gmail.com>
2023-06-23 17:48:49 +0200

    basics3 updated

--- Python_Basics3.py
+++ Python_Basics3.py
@@ -21,3 +21,23 @@ n.do()
 
 # to find the path of the inheritance we can print : print (D.mro())
 print (D.mro())
+# in our case, the super or the bais class is B because the code stoped on B when it found the "do" in it
+
+######## EX :
+
+'''
+create student
+add marks
+get avg
+
+'''
+class Student ():
+    #def create_student (self,name): or we can add a constructor and delet the
+    #object by defining the "self" s1.creat_student(''Wael)
+    def __init__(self,name):
+        print (f'Welcome {name}')
+
+#class marks
+
+s1 = Student('Wael')
+#s1.create_student ('Wael')
